8a62898530aad78b7e32a18995256463912993f0
Jady Lima <jadylsilva@gmail.com>
2023-03-31 10:14:11 -0300

    Create NumberDisplayView.java

---
+++ Aulas/Aula01/NumberDisplayView.java
@@ -0,0 +1,14 @@
+public class NumberDisplayView{
+    public static void main (String arg[]){
+        NumberDisplay numberDisplay = new NumberDisplay();
+
+        //CHAMA OS MÉTODOS DE MODIFICAR AS VARIAVEIS
+        numberDisplay.setLimit(2);
+        numberDisplay.setValue(4);
+
+        //IMPRIME OS VALORES NOS METODOS DE ACESSO 
+        System.out.println("Limite: " + numberDisplay.getLimit());
+        System.out.println("Valor: " + numberDisplay.getValue());
+    }
+    
+}
