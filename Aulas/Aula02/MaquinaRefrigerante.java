71e8d80d83017199a1eb620a640a719a2b809fe8
Jady Lima <jadylsilva@gmail.com>
2023-03-31 13:01:30 -0300

    MaquinaRefrigerante update

--- Aulas/Aula02/MaquinaRefrigerante.java
+++ Aulas/Aula02/MaquinaRefrigerante.java
@@ -1,5 +1,3 @@
-package Aula02;
-
 public class MaquinaRefrigerante
 {
     //ATRIBUTOS 
@@ -14,16 +12,16 @@ public class MaquinaRefrigerante
         total = 0;
     }
 
-    public MaquinaRefrigerante() {
-        preco = 0;
-        balanco = 0;
-        total = 0;
-    }
-
     //METODO DE RETORNO DO PRECO (get)
     public int getPreco()
     {
         return preco;
+    }
+
+    //METODO DE EDIÇÃO DO PRECO
+    public void setPreco(int p)
+    {
+        preco = p;
     }
 
     //METODO DE RETORNO DE BALANÇO
@@ -32,10 +30,22 @@ public class MaquinaRefrigerante
         return balanco;
     }
 
+    //MÉTODO DE EDIÇÃO DO BALANÇO
+    public void setBalanco(int b)
+    {
+        balanco = b;
+    }
+
     //MÉTODO DE RETORNO DO TOTAL
     public int getTotal()
     {
         return total;
+    }
+
+    //MÉTODO DE EDIÇÃO DO TOTAL
+    public void setTotal(int t)
+    {
+        total = t;
     }
 
     //MÉTODO QUE RECEBE UM VALOR EM DINHEIRO
@@ -48,7 +58,7 @@ public class MaquinaRefrigerante
     public void imprimirPreco()
     {
         System.out.println("####################");
-        System.out.println("# PREÇO            #");
+        System.out.println("# PRECO            #");
         System.out.println("# REFRIGERANTE:    #");
         System.out.println("# R$ " + preco + ",00          #");
         System.out.println("####################");
--- Aulas/Aula02/MaquinaRefrigeranteView.java
+++ Aulas/Aula02/MaquinaRefrigeranteView.java
@@ -1,12 +1,19 @@
-package Aula02;
-
 public class MaquinaRefrigeranteView 
 {
-    public static void main(String args[]) {
+    public static void main (String arg[]) 
+    {
         //INSTANCIANDO A CLASSE
         MaquinaRefrigerante maquina = new MaquinaRefrigerante(5);
 
         //CHAMANDO O METODO DE IMPRESSÃO
         maquina.imprimirPreco();
+
+        //INSERIR DINHEIRO NA MAQUINA
+        System.out.println("");
+        maquina.inserirDinheiro(20);
+
+        //RETORNANDO O BALANÇO CORRENTE
+        System.out.println("");
+        System.out.println("Balanco atual => " + maquina.getBalanco());
     }
 }
