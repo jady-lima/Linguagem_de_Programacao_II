cfc98e13a7fa83c27e143ef4752fd973aec98812
Jady Lima <jadylsilva@gmail.com>
2023-03-31 11:18:47 -0300

    Create MaquinaRefrigeranteView.java

---
+++ Aulas/Aula02/MaquinaRefrigeranteView.java
@@ -0,0 +1,11 @@
+package Aula02;
+
+public class MaquinaRefrigeranteView {
+    public static void main(String args[]) {
+        //INSTANCIANDO A CLASSE
+        MaquinaRefrigerante maquina = new MaquinaRefrigerante(2);
+
+        //CHAMANDO O METODO DE IMPRESSÃO
+        maquina.imprimirPreco();
+    }
+}
