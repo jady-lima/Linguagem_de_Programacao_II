91dba59939a35c628d1d87cd82da37c76e7c1c52
Jady Lima <jadylsilva@gmail.com>
2023-03-31 21:02:47 -0300

    Update SistemaEscolarView.java

--- Atividades/Atividade02/SistemaEscolarView.java
+++ Atividades/Atividade02/SistemaEscolarView.java
@@ -1,6 +1,8 @@
 public class SistemaEscolarView {
     public static void main(String[] args) {
-        Turma turma = new Turma(4, 2.5, 5, 3, 4.5, 5, 2, 1, 6, 1.5, 6, 3);
+        Turma turma = new Turma(4, 2.5, 5, 3, 
+                                4.5, 5, 2, 1, 
+                                6, 1.5, 6, 3);
 
         turma.mostraAluno(0);
         turma.mostraAluno(1);
