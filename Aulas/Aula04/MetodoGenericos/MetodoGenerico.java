cdc0136b2f54ed8255a0507ef3be3dc4ed9f681b
Jady Lima <jadylsilva@gmail.com>
2023-04-06 12:18:21 -0300

    Create MetodoGenerico.java

---
+++ Aulas/Aula04/MetodoGenericos/MetodoGenerico.java
@@ -0,0 +1,17 @@
+public class MetodoGenerico 
+{
+    //Arrays de diversos tipos
+    Double [] arrayDouble = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+    Integer [] arrayInt = {1, 2, 3, 4, 5, 6};
+    Character [] arrayChar = {'O', 'L', 'A', '!'};
+
+    //Método generico
+    public <T> void imprimirVetor(T v[])
+    {
+        for (T o : v) 
+        {
+            System.out.println(o + " ");
+        }
+        System.out.println();
+    }
+}
