6438e26939f1c51f01a2de66b83ca0c725875681
Jady Lima <jadylsilva@gmail.com>
2023-04-11 16:29:46 -0300

    Create ProdutoDuravel.java

---
+++ Atividades/Atividade03/ProdutoDuravel.java
@@ -0,0 +1,23 @@
+public class ProdutoDuravel extends Produto
+{
+    //Atributos
+    private int durabilidade;
+
+    //construtor
+    public ProdutoDuravel()
+    {
+
+    }
+
+    //método de acesso da durabilidade 
+    public int getDurabilidade()
+    {
+        return durabilidade;
+    }
+
+    //método de edição da durabilidade
+    public void setDurabilidade(int d)
+    {
+        durabilidade = d;
+    }
+}
