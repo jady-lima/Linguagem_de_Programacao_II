e14591c0b610cacb82bd9a0ab014fc5c53b87dcc
Jady Lima <jadylsilva@gmail.com>
2023-04-14 18:19:35 -0300

    Create Professores.java

---
+++ Atividades/Atividade04/Professores.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class Professores extends Funcionarios{
+    //Atributos
+    private boolean natacao = false;
+    private boolean musculacao = false;
+
+    //Array de alunos
+    private ArrayList<Alunos> alunos;
+
+    //Construtor da classe
+    public Professores()
+    {
+        alunos = new ArrayList<Alunos>();
+    }
+
+    //Método que adiciona um novo aluno
+    public void addAluno(Alunos a)
+    {
+        alunos.add(a);
+    }
+
+    //Método que remove um aluno por indice
+    public void removeAluno(int i)
+    {
+        if (i <  0)
+        {
+            System.out.println("Numero invalido.");
+        }
+        else if (i < quantidadeAlunos())
+        {
+            alunos.remove(i);
+        }
+        else
+        {
+            System.out.println("Numero invalido.");
+        }
+    }
+
+    //Método que retorna a quantidade de alunos do professor
+    public int quantidadeAlunos()
+    {
+        return alunos.size();
+    }
+
+    //Método de retorno da natação
+    public boolean getNatacao()
+    {
+        return natacao;
+    }
+
+    //Método de edição da natação
+    public void setNatacao(boolean n)
+    {
+        natacao = n;
+    }
+
+    //Método de acesso de musculação
+    public boolean getMusculacao()
+    {
+        return musculacao;
+    }
+
+    public void setMusculacao(boolean m)
+    {
+        musculacao = m;
+    }
+}
