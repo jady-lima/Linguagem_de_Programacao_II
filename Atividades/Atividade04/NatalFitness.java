c6a84f71aca09249a2113c4d3c9f9ec97a51e0d7
Jady Lima <jadylsilva@gmail.com>
2023-04-14 18:20:34 -0300

    Create NatalFitness.java

---
+++ Atividades/Atividade04/NatalFitness.java
@@ -0,0 +1,15 @@
+public class NatalFitness {
+    public static void main(String[] args) {
+        //Funcionarios do tipo professor
+        Funcionarios professor1 = new Professores();
+        Funcionarios professor2 = new Professores();
+
+        //Funcionarios do tipo Recepcionista
+        Funcionarios recepcionista1 = new Recepcionistas();
+        Funcionarios recepcionista2 = new Recepcionistas();
+
+        //Objetos do tipo Aluno
+        Alunos aluno1 = new Alunos();
+        Alunos aluno2 = new Alunos();
+    }
+}
