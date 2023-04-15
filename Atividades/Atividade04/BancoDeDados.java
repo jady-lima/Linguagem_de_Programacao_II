660ee15d18f35d1627af997d44dca50bb70b3834
Jady Lima <jadylsilva@gmail.com>
2023-04-15 13:59:06 -0300

    Update BancoDeDados.java

--- Atividades/Atividade04/BancoDeDados.java
+++ Atividades/Atividade04/BancoDeDados.java
@@ -1,7 +1,12 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class BancoDeDados 
 {
+    //Data atual
+    private Calendar calendar;
+
     //Array de funcionarios
     private ArrayList<Funcionarios> funcionarios;
     private ArrayList<Alunos> alunos;
@@ -10,6 +15,8 @@ public class BancoDeDados
     public BancoDeDados()
     {
         funcionarios = new ArrayList<Funcionarios>();
+        alunos = new ArrayList<Alunos>();
+        calendar = Calendar.getInstance();
     }
 
     //Função que adiciona um funcionário ao array
@@ -44,52 +51,51 @@ public class BancoDeDados
     //Função que retorna o relatótio de funcionarios
     public void listaFuncionarios()
     {
-        System.out.println("#################### FUNCIONARIOS ####################");
+        System.out.println("############################# FUNCIONARIOS ##############################");
         for (Funcionarios funcionario : funcionarios) 
         {
-            System.out.println("Matricula: " + funcionario.getMatricula() + " Nome: " + funcionario.getNome() + " Tipo: " + funcionario.getClass());
+            System.out.println("| Matricula: " + funcionario.getMatricula() + " | Nome: " + funcionario.getNome() + " | Tipo: " + funcionario.getClass() + "\t|");
         }
-        System.out.println("######################################################");
+        System.out.println("#########################################################################\n");
     }
 
     //Função que retorna o relatório de professores
     public void listaProfessores()
     {
-        System.out.println("#################### PROFESSORES #####################");
+        System.out.println("############################## PROFESSORES ##############################");
         for (Funcionarios funcionario : funcionarios) 
         {
             if (funcionario instanceof Professores)
             {
-                System.out.println("Professor: " + ((Professores)funcionario).getNome());
+                System.out.println("| PROFESSOR: " + ((Professores)funcionario).getNome() + "\t\t\t\t\t|");
                 ((Professores)funcionario).listaAlunos();
             } 
         }
-        System.out.println("######################################################");
+        System.out.println("#########################################################################\n");
     }
 
-    //Função que retorna o relatório de recepcionistas
+    //Função que retorna o relatório de professores
     public void listaRecepcionistas()
     {
-        System.out.println("################### RECEPCIONISTAS ###################");
+        System.out.println("########################### RECEPCIONISTAS ##############################");
         for (Funcionarios funcionario : funcionarios) 
         {
-            if (funcionario instanceof Recepcionistas) 
+            if (funcionario instanceof Recepcionistas)
             {
-                System.out.println("Nome: " + ((Recepcionistas)funcionario).getNome());
-                ((Recepcionistas)funcionario).listaHorarios();   
-            }
+                System.out.println("| RECEPCIONISTA: " + ((Recepcionistas)funcionario).getNome() + "\t\t\t\t\t|");
+                ((Recepcionistas)funcionario).listaHorarios();
+            } 
         }
-        System.out.println("######################################################");
-
+        System.out.println("#########################################################################\n");
     }
 
-    //Função que cria um array de alunos
+    //Função que adiciona um aluno ao vetor de alunos
     public void addAluno(Alunos a)
     {
         alunos.add(a);
     }
 
-    //Função que remove um aluno
+    //Função que remove um aluno do vetor pelo indice
     public void removeAluno(int i)
     {
         if (i < 0)
@@ -106,7 +112,7 @@ public class BancoDeDados
         }
     }
 
-    //Função que retorna a quantidade de alunos total no vetor
+    //Função que retorna a quantidade de alunos
     public int quantidadeAlunos()
     {
         return alunos.size();
@@ -115,10 +121,14 @@ public class BancoDeDados
     //Função que retorna o relatótio de alunos
     public void listaAlunos()
     {
-        System.out.println("####################### ALUNOS #######################");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy");
+        int idade = calendar.get(Calendar.YEAR);
+        
+        System.out.println("################################ ALUNOS #################################");
         for (Alunos aluno : alunos) 
         {
-            System.out.println("Nome: " + aluno.getNome() + " Nascimento: " + aluno.getNascimento());    
+            System.out.println("| Nome do aluno >>> " + aluno.getNome() + " | Idade: " + (idade - Integer.parseInt(formato.format(aluno.getNascimento()))) + " | CPF: " + aluno.getCPF() + "\t|");
         }
+        System.out.println("#########################################################################\n");
     }
 }
